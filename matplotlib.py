279c07bfb1c17b2378a74915dac88d4f28c0a57a
Ali husnain <ahu8165@gmail.com>
2023-10-03 12:36:58 +0500

    matplotlib task1

--- matplotlib.py
+++ matplotlib.py
@@ -1,35 +1,15 @@
+# importing packages
 import numpy as np
+from matplotlib.pyplot import *
 
-# create a two array with different variable 
-arr_1 = np.array([9,3,5,6,78,2])
-arr_2 = np.array([9,3,7,5,78,2])
+# create data
+x = np.linspace(1, 50, 50)
+np.random.seed(1)
+y = np.random.randint(0, 20, 50)
 
-# rearrange the list into the colums stack.
-arrange_colum = np.column_stack((arr_1,arr_2))
-print(arrange_colum)
+# plot graph
+plt.plot(x, y)
 
-# rearrange the list into the row stack
-arrange_rom = np.row_stack((arr_1,arr_2))
-print(arrange_rom)
-
-# use the roll method in numpy
-arr_1[1] = np.roll(arr_1[1],1)
-
-print ("use the roll method in list :",arr_1)
-
-
-
-# Remove the index from the list 
-list = [9,4,5,7,9]
-list.remove(4)
-print(list)
-
-# Checkt the type.
-print(type(type(bool('1'))))
-
-# check the index of the list
-Check_data = list.index(7)
-print(Check_data)
-
-print(False * bool(3.2))
-print(3*"hellow")
+# add legend
+plt.legend(['Legend'])
+plt.show()
