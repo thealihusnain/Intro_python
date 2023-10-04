71c299249b988a06db6feb0eb047ef46b13b02da
Ali husnain <ahu8165@gmail.com>
2023-10-04 14:36:53 +0500

    add the one dictionary to another dict

--- Related_dictionary.py/Value_dict.py
+++ Related_dictionary.py/Value_dict.py
@@ -26,9 +26,46 @@ europe['italy'] = 'rome'
 
 # Print out italy in europe
 
-print(europe['italy'])
+print('italy' in europe)
 # Add poland to europe
 europe['poland'] = 'warsaw'
 
 # Print europe
 print(europe)
+
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+europe.pop('australia')
+
+
+# Print europe
+print(europe)
+# ---------------------------------------------------------------------------------------------------------------------------------
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital': 'rome','po;ulation':59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
+
