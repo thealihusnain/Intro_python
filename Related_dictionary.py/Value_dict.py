751fd96e7f1807bd24be6454a69ed1f8a711569f
Ali husnain <ahu8165@gmail.com>
2023-10-04 14:19:58 +0500

    add the key value into the dictionary

--- Related_dictionary.py/Value_dict.py
+++ Related_dictionary.py/Value_dict.py
@@ -17,3 +17,18 @@ for key,value in europe.items():
 list_of_key = list(europe.keys())
 
 print(list_of_key)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+
+print(europe['italy'])
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
