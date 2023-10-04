457d2fe37e0599166fd768fd8a8eb32ed308a29d
Ali husnain <ahu8165@gmail.com>
2023-10-04 17:09:02 +0500

    read the csv file and apply some technique on the data

--- Related_dictionary.py/Value_dict.py
+++ Related_dictionary.py/Value_dict.py
@@ -91,3 +91,41 @@ cars = pd.DataFrame(my_dict)
 
 # Print cars
 print(cars)
+
+# -------------------------------------------------------------
+
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+# Print cars again
+print(cars)
+
+
+
+# ------------------------------------------------------------------------------------------------------
+
+# Import cars data
+import pandas as pd
+company_sales_data = pd.read_csv('company_sales_data.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+print(company_sales_data['total_profit'])
+
+# Print out country column as Pandas DataFrame
+# print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+print(company_sales_data[['total_units','total_profit']])
