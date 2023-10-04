4479246781ef166d4c712315e0aedd0d104f04bc
Ali husnain <ahu8165@gmail.com>
2023-10-04 12:49:51 +0500

    Add the dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -4,6 +4,7 @@ import numpy as np
 countries = ['spain', 'france', 'germany', 'norway']
 capitals = ['madrid', 'paris', 'berlin', 'oslo']
 
+
 # Get the index of the germany from the countries list
 Get_index = countries.index('norway')
 
@@ -14,4 +15,16 @@ Country_Name = countries[Get_index]
 Get_capital = capitals[Get_index]
 
 # Print out the captials of the germany
-print(f"Capital of the {Country_Name} Country is :",Get_capital)
+# print(f"Capital of the {Country_Name} Country is :",Get_capital)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+key_europ = europe.keys()
+value_of_key_europ = europe.values()
+print("key and value of the europ dict :", key_europ ,value_of_key_europ)
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+
